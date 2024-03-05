361520da7173ad6e64b1acc52622856bdf3225c7
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-03-05 20:27:38 +0530

    Register new user with validations

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,28 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const userRoute = require("./routes/userRoute")
+
+const errorHandler = require("./middleWare/errorMiddleware");
+
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
+
+//Routes Middleware
+app.use("/api/users", userRoute);
+
+//routes
 app.get("/", (req,res) => {
     res.send("Home Page");
 });
+
+//Error Midleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5566;
 
@@ -21,8 +34,8 @@ mongoose
 .then(() => {
     app.listen(PORT, () => {
         console.log(`Server running on port : ${PORT}`)
-    })
+    });
 })
-.catch((err) =>console.log(err))
+.catch((err) =>console.log(err));
 
 
