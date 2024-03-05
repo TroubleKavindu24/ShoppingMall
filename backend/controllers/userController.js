8a2804f449642162bba63b887c7b6b5f39306926
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-03-05 23:04:01 +0530

    Generate JSON Web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
+};
 
 
 const registerUser =asyncHandler( async (req, res) => {
@@ -29,12 +34,21 @@ const registerUser =asyncHandler( async (req, res) => {
         name,
         email,
         password,
-    })
+    });
+
+    //genetare token
+    const token = generateToken(user._id)
 
     if(user){
         const {_id, name, email, photo, phone, bio} = user
         res.status(201).json({
-            _id, name, email, photo, phone, bio
+            _id, 
+            name, 
+            email, 
+            photo, 
+            phone, 
+            bio,
+            token,
         })
     }else{
         res.status(400)
