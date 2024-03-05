81706e5446dc68ea259c706cbf531f599544b6b7
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-03-05 12:34:59 +0530

    Create user model

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,25 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express()
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get("/", (req,res) => {
+    res.send("Home Page");
+});
 
 const PORT = process.env.PORT || 5566;
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
     app.listen(PORT, () => {
         console.log(`Server running on port : ${PORT}`)
     })
 })
 .catch((err) =>console.log(err))
+
+
