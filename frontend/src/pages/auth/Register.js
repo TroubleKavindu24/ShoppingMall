e6aba815388a904d2119124fa3d06dc5fd2f8191
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-03-23 01:07:47 +0530

    register page

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React from 'react';
+import styles from "./auth.module.scss";
+import { TiUserAddOutline } from "react-icons/ti";
+import Card from '../../components/card/Card';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
   return (
-    <h1>Register</h1>
+    <div className={`container ${styles.auth}`}>
+        <Card>
+            <div className={styles.form}>
+                <TiUserAddOutline size={35} color='#999'/>
+            </div>
+            <h2>
+                Register
+            </h2>
+
+            <form>
+                <input 
+                    type='text' 
+                    placeholder='Name' 
+                    required name="name"
+                /><br/>
+                <input 
+                    type='email' 
+                    placeholder='email' 
+                    required name="email"
+                /><br/>
+                <input 
+                    type='password' 
+                    placeholder='password' 
+                    required name="password"
+                /><br/>
+                <input 
+                    type='password' 
+                    placeholder='confirm password' 
+                    required name="password"
+                />
+                <button 
+                    type='submit' 
+                    className='--btn --btn-primary --btn-block'>
+                    Register
+                </button>
+            </form>
+            <span className={styles.register}>
+                <Link to="/">Home</Link>
+                <p> &nbsp; Already have an account? &nbsp;</p>
+                <Link to='/login'>Login</Link>
+            </span>
+        </Card>
+    </div>
   )
 }
 
