107f9ec84fdd61abef1d954a0476e9cd0da0a8fc
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-03-23 01:25:12 +0530

    Frogot passsword page

--- frontend/src/pages/auth/Forgot.js
+++ frontend/src/pages/auth/Forgot.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styles from "./auth.module.scss";
+import { AiOutlineMail } from "react-icons/ai";
+import Card from '../../components/card/Card';
+import { Link } from 'react-router-dom';
+
 
 const Forgot = () => {
   return (
-    <h1>Forgot</h1>
+    <div className={`container ${styles.auth}`}>
+        <Card>
+            <div className={styles.form}>
+                <AiOutlineMail size={35} color='#999'/>
+            </div>
+            <h2>
+                Forgot password
+            </h2>
+
+            <form>
+                <input 
+                    type='email' 
+                    placeholder='Email' 
+                    required name="email"
+                /><br/>
+                <button 
+                    type='submit' 
+                    className='--btn --btn-primary --btn-block'>
+                    Get Reset Email
+                </button>
+                <div className={styles.links}>
+                  <p>
+                    <Link to="/">- Home</Link>
+                  </p>
+                  <p>
+                    <Link to="/login">- Login</Link>
+                  </p>
+                </div>
+            </form>
+
+        </Card>
+    </div>
   )
 }
 
